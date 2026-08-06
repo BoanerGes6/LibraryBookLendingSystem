d07556ad454b06d116f07aa274274e32161bcdce
BoanerGes6 <insanerunner111@gmail.com>
2026-08-06 12:17:24 +0530

    initial Commit

--- src/main/java/com/LibraryBook/controller/MemberController.java
+++ src/main/java/com/LibraryBook/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.LibraryBook.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.LibraryBook.entity.Member;
@@ -15,19 +17,20 @@ import jakarta.validation.Valid;
 @RequestMapping("/api")
 public class MemberController {
 
-	private final MemberService memberSevice;
+	private final MemberService memberService;
 	
 	public MemberController(MemberService memberService) {
-		this.memberSevice = memberService;
+		this.memberService = memberService;
 	}
 	
 	@PostMapping("/add")
 	public ResponseEntity<?> Register(@Valid @RequestBody Member member) {
 		
-		return memberSevice.addMember(member);
+		return memberService.addMember(member);
 	}
 	
-	public void updateMember() {
-		
+	@PostMapping("/borrowBook")
+	public void borrowBook(@RequestParam Long bookId) {
+		memberService.borrow(bookId);
 	}
 }
--- src/main/java/com/LibraryBook/service/MemberService.java
+++ src/main/java/com/LibraryBook/service/MemberService.java
@@ -1,9 +1,13 @@
 package com.LibraryBook.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.LibraryBook.entity.Book;
 import com.LibraryBook.entity.Member;
+import com.LibraryBook.exceptions.BookNotFound;
+import com.LibraryBook.repository.BookRepository;
 import com.LibraryBook.repository.MemberRepository;
 
 @Service
@@ -15,10 +19,19 @@ public class MemberService {
 		this.memberRepo = memberRepo;
 	}
 
+	@Autowired
+	private BookRepository bookRepo;
+	
 	public ResponseEntity<?> addMember(Member member) {
 		
 		Member saved = memberRepo.save(member);
 		
 		return ResponseEntity.ok(saved);
+	}
+	
+	public void borrow(Long bookId) {
+		
+		Book book = bookRepo.findById(bookId).orElseThrow(() -> 
+				new BookNotFound("Book Not Found or Currently Unavailable"));
 	}
 }
